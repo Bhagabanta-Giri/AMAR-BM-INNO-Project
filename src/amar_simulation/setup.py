4b930f2036a207fde38a63f69f8d5650a4be65eb
Bhagabanta-Giri <bhagabantagiri@gmail.com>
2026-08-28 23:52:52 +0530

    Add new simulation worlds for TurtleBot3 DQN stages and house environment
    - Created `turtlebot3_dqn_stage1.world` with basic setup and ground plane model.
    - Created `turtlebot3_dqn_stage2.world` with obstacles included and IMU plugin.
    - Created `turtlebot3_dqn_stage3.world` with additional obstacles and a plugin for obstacle management.
    - Created `turtlebot3_dqn_stage4.world` with inner walls and multiple obstacle plugins.
    - Created `turtlebot3_house.world` with a static house model and sun included.
    - Created `turtlebot3_world.world` with a static world setup and sun included.

--- src/amar_simulation/setup.py
+++ src/amar_simulation/setup.py
@@ -1,5 +1,6 @@
-from setuptools import find_packages, setup
 import os
+from glob import glob
+from setuptools import find_packages, setup
 
 package_name = 'amar_simulation'
 
@@ -11,6 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),
+        # [(os.path.join('share', package_name, root), [os.path.join(root, f) for f in files]) for root, dirs, files in os.walk('models') if files],
+        (os.path.join('share', package_name, 'rviz/'),
+            glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'urdf/'),
+            glob('urdf/*.urdf.xacro')),
+        (os.path.join('share', package_name, 'worlds/'),
+            glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
