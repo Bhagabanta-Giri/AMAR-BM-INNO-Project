65fc68db5ef23b343e57160adb82a1723513ebc3
Bhagabanta-Giri <bhagabantagiri@gmail.com>
2026-08-29 00:41:19 +0530

    Add new meshes and URDF files for the TurtleBot3 Burger model
    - Added R200 sensor image to the meshes directory.
    - Included left and right tire STL files for the TurtleBot3 wheels.
    - Created RViz configuration file to visualize the robot model.
    - Defined common material properties in a new URDF file.
    - Implemented the TurtleBot3 Burger URDF with links, joints, and visual elements.

---
+++ src/amar_description/launch/robot_state_publisher.launch.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import UnlessCondition
+from launch_ros.actions import Node
+import xacro, urdf
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf_file_name = 'turtlebot3_burger.urdf'
+
+    # print('xacro_file_name : {}'.format(urdf_file_name))
+
+    xacro_file = os.path.join(
+        get_package_share_directory('amar_description'),
+        'urdf',
+        urdf_file_name)
+    robot_description_config = urdf.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        
+       Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'robot_description': robot_urdf,
+                'publish_joints': True
+            }],
+        ),
+    ])
